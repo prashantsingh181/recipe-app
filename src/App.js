f5e6fcebac7a72c9cea471abd907e0adf83f4a91
prashantsingh181 <prashant.singh181@gmail.com>
2024-04-21 13:51:11 +0530

    added final chnages

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import RecipeDetails from "./pages/RecipeDetails";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="favorites" element={<Favorites />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Route>
-    </Routes>
+    <BrowserRouter basename="/recipe-app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
